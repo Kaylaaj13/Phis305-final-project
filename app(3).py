90bebdef8e67eb0dc89053b7bf4f2ec543d282ba
Kaylaaj13 <jeffriesk0811@students.bowiestate.edu>
2025-05-18 22:45:13 -0400

    Update app(3).py

--- app(3).py
+++ app(3).py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title(" Interactive Data Dashboard")
+st.title(" High Blood Pressure Interactive Data Dashboard")
 
 #create file upload widget (an interactive UI element that allow users to provide input)
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
@@ -10,15 +10,15 @@ if uploaded_file is not None:
     st.write("File uploaded...")
     df = pd.read_csv(uploaded_file)
 
-    st.subheader("Data Preview")
+    st.subheader("Blood Pressure Data Charts")
     st.write(df.head())
     st.write(df.tail())
     
-    st.subheader("Data Summary")
+    st.subheader("Data Chart Viewer")
     st.write(df.describe()) 
     # gives a summary of the statistical properties on numerical data in the df dataframe eg. count, std(spread of the data), 25% - 25% of data falls below xx value
     
-    st.subheader("Filter Data")
+    st.subheader("Filter Data Chart Viewer")
     columns = df.columns.tolist() #get all column names from the dataframe df as a simple list or regular python list.
     selected_column = st.selectbox("Select colum to filter by", columns)
 
@@ -28,7 +28,7 @@ if uploaded_file is not None:
     filtered_df = df[df[selected_column] == selected_value] #filter the df to show only the rows where the values in a specific column matches a certain value.
     st.write(filtered_df)
 
-    st.header("Plot Data")
+    st.header("Line Chart Of slected Columns")
     x_column = st.selectbox("Select x_axis column", columns)
     y_column = st.selectbox("Select y_axis column", columns)
 
